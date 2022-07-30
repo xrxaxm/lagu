d3b9a353eafd381f7b04bfb267caac4f1d07d98f
xrxaxm <69647243+xrxaxm@users.noreply.github.com>
2022-07-30 23:40:53 +0700

    Create ram.js

---
+++ ram.js
@@ -0,0 +1,129 @@
+
+
+var sounds = new Array();
+
+         sounds[0]="https://dl.dropbox.com/s/p0r9pao5zaqbpab/11%20Only%20Today.mp3?dl=0" 
+
+         type = "audio/mp3";
+
+         sounds[1]="https://dl.dropbox.com/s/eeqbepfn8v5ug6p/01.%20Hanya%20Lihat%20Ke%20Depan%20%28Mae%20Shika%20Mukanee%29.mp3?dl=0";
+
+         sounds[2]="https://dl.dropbox.com/s/bszxgpe3o5qc9go/jkt48%20-%20%20gingham%20check.mp3?dl=0";
+
+         sounds[3]="https://dl.dropbox.com/s/d34z2xu214b2iml/jkt48%20-%20heavy%20rotation.mp3?dl=0";
+
+         sounds[4]="https://dl.dropbox.com/s/zatk8kkijh29w0b/Yumesekai.mp3?dl=0";
+
+         sounds[5]="https://dl.dropbox.com/s/qp4x5mf5w5u42o9/Anokoro%20-Jin%20Jin%20Bao%20Zhuo%20Ni-.m4a?dl=0";
+
+         sounds[6]="https://dl.dropboxusercontent.com/s/i0k4usxbch11dqd/09.%20%20Sign%20%28Audio%29%20-%20FLOW%20-%20FLOW%20ANIME%20BEST.mp3?dl=0";
+
+         sounds[7]="https://dl.dropbox.com/s/y63q1fgvbodizco/10%20%E5%90%9B%E3%81%AE%E8%A1%97%E3%81%B8.mp3?dl=0";
+
+         sounds[8]="https://dl.dropboxusercontent.com/s/h33tztiizx3be99/%E3%81%82%E3%81%AE%E5%A4%A2%E3%82%92%E3%81%AA%E3%81%9E%E3%81%A3%E3%81%A6.mp3?dl=0";
+
+         sounds[9]="https://dl.dropbox.com/s/pxbdq3oya89h4in/%E3%81%9F%E3%81%B6%E3%82%93.mp3?dl=0";
+
+         sounds[10]="https://dl.dropbox.com/s/vle9bdtfp0ie6er/%E3%83%A9%E3%83%96%E3%83%AC%E3%82%BF%E3%83%BC.mp3?dl=0";
+
+         sounds[11]="https://dl.dropbox.com/s/v7ee2vl289d8ez3/%E3%82%A2%E3%83%B3%E3%82%B3%E3%83%BC%E3%83%AB.mp3?dl=0";
+
+         sounds[12]="https://dl.dropbox.com/s/0xucf9pf5g0klqv/%E7%BE%A4%E9%9D%92.mp3?dl=0";
+
+         sounds[13]="https://dl.dropbox.com/s/bp24kpacpqpchxk/%E3%83%8F%E3%83%AB%E3%82%AB.mp3?dl=0";
+
+         sounds[14]="https://dl.dropbox.com/s/bp24kpacpqpchxk/%E3%83%8F%E3%83%AB%E3%82%AB.mp3?dl=0";
+
+         sounds[15]="https://dl.dropbox.com/s/ek5wxzu54ssulb4/comet.mp3?dl=0";
+
+         sounds[16]="https://dl.dropbox.com/s/ttvj0lb7mxx6w80/%E3%83%8F%E3%83%AB%E3%82%B8%E3%82%AA%E3%83%B3.mp3?dl=0";
+
+         sounds[17]="https://dl.dropbox.com/s/l9w69f0upxpvzpd/%E3%82%82%E3%81%86%E5%B0%91%E3%81%97%E3%81%A0%E3%81%91.mp3?dl=0";
+
+         sounds[18]="https://dl.dropbox.com/s/il8sd8yxozmprtp/%E3%80%90%EF%BC%93%E6%9C%88%E3%81%AE%E3%83%A9%E3%82%A4%E3%82%AA%E3%83%B3%E3%80%91orion-acoustic%20arrange-%E3%80%94%E6%AD%8C%E3%81%A3%E3%81%A6%E3%81%BF%E3%81%9F%E3%80%95%E3%80%90marie%E2%98%86fd%E3%80%91%28mp3_320k%29.mp3?dl=0";
+
+         sounds[19]="https://dl.dropbox.com/s/oxi1p81d653yt8l/%E3%80%90mad%E3%80%91%E5%96%9C%E6%AD%A1%E4%B8%8A%E4%BD%A0%E7%9A%84%E9%82%A3%E4%B8%80%E5%A4%A9%28mp3_320k%29.mp3?dl=0";
+
+         sounds[20]="https://dl.dropbox.com/s/rztt0ckv0fkb3vw/%E3%80%90yakusoku%20%E7%B4%84%E6%9D%9F%E3%80%91akie%E7%A7%8B%E7%B5%B5%28mp3_320k%29.mp3?dl=0";
+
+         sounds[21]="https://dl.dropbox.com/s/zu5bk2wz090lc9w/%E3%80%90%E5%85%AC%E5%BC%8F%E3%80%91h%E2%96%B3g%E3%80%8C%20%E5%8D%92%E6%A5%AD%E3%81%AE%E5%94%84%20%E3%80%8Dmusic%20video%EF%BC%88%20%E3%82%A2%E3%83%AB%E3%83%90%E3%83%A0%E3%80%8C%E7%9E%AC%E3%81%8D%E3%82%82%E3%81%9B%E3%81%9A%E3%81%AB%20%E3%80%8D%E5%8F%8E%E9%8C%B2%E6%9B%B2%20%EF%BC%89%28mp3_320k%29.mp3?dl=0";
+
+         sounds[22]="https://dl.dropbox.com/s/1vclnvtzpv1hrht/%E3%80%90%E5%85%AC%E5%BC%8F%E3%80%91h%E2%96%B3g%E3%80%8C%20%E5%90%9B%E3%81%AE%E3%81%BE%E3%81%BE%E3%81%A7%E3%81%84%E3%81%84%20%E3%80%8Dmusic%20video%EF%BC%88%202%E6%9C%8824%E6%97%A5%E7%99%BA%E5%A3%B2%E3%80%8C%E7%9E%AC%E3%81%8D%E3%82%82%E3%81%9B%E3%81%9A%E3%81%AB%20%E3%80%8D%E5%8F%8E%E9%8C%B2%E6%9B%B2%20%EF%BC%89%28mp3_320k%29.mp3?dl=0";
+
+         sounds[23]="https://dl.dropbox.com/s/xzy6cs0eifr9fns/%E3%80%90%E5%A5%B3%E6%80%A7%E3%81%8C%E6%AD%8C%E3%81%86%E3%80%91%E7%81%B0%E8%89%B2%E3%81%A8%E9%9D%92%EF%BC%8B%E8%8F%85%E7%94%B0%E5%B0%86%E6%9A%89_%E7%B1%B3%E6%B4%A5%E7%8E%84%E5%B8%AB%28covered%20by%20%E3%82%B3%E3%83%90%E3%82%BD%E3%83%AD%20_%20%E6%98%A5%E8%8C%B6%29%28mp3_320k%29.mp3?dl=0";
+
+         sounds[24]="https://dl.dropbox.com/s/tabft1hcuh11yl9/7__%20-%20lovers%28mp3_320k%29.mp3?dl=0";
+
+         sounds[25]="https://dl.dropbox.com/s/0bcue7tsyw1sgqc/aimer%20%E3%80%8Eref_rain%E3%80%8Fmusic%20video%285th%20album%E3%80%8Esun%20dance%E3%80%8F%E3%80%8Epenny%20rain%E3%80%8F2019_04_10%EF%BC%88%E6%B0%B4%EF%BC%892%E6%9E%9A%E5%90%8C%E6%99%82%E7%99%BA%E5%A3%B2%29%28mp3_320k%29.mp3?dl=0";
+
+         sounds[26]="https://dl.dropbox.com/s/unju5m1n5caw37u/asca%20%E3%80%8Cchain%E3%80%8D%20%28anime%E3%80%8C%E3%83%80%E3%83%BC%E3%82%A6%E3%82%A3%E3%83%B3%E3%82%BA%E3%82%B2%E3%83%BC%E3%83%A0%28darwins%20game%29%E3%80%8Dop%29%28mp3_320k%29.mp3?dl=0";
+
+         sounds[27]="https://dl.dropbox.com/s/we7sqiz7r5vq9l4/hanatan%20-%20hana%20no%20uta%28mp3_320k%29.mp3?dl=0";
+
+         sounds[28]="https://dl.dropbox.com/s/4flnt4ey3oxv0bo/kano%20%E2%86%92%E3%80%8C%20glow%20%E3%80%8D%28mp3_320k%29.mp3?dl=0";
+
+         sounds[29]="https://dl.dropbox.com/s/krsbpzc9iho49mt/kobo%20kanaeru%20-%20mantra%20hujan%20%28%20lhntrx%20lofi%20edit%20%29%28mp3_320k%29.mp3?dl=0";
+
+         sounds[30]="https://dl.dropbox.com/s/uwtyvubjx1duye7/lisa%20-%20crossing%20field%28mp3_320k%29.mp3?dl=0";
+
+         sounds[31]="https://dl.dropbox.com/s/qyqhjw87dsusu2v/lisa%20-%20rising%20hope%28mp3_320k%29.mp3?dl=0";
+
+         sounds[32]="https://dl.dropbox.com/s/67gw3e7mnqf9tzi/lisa%20%E3%80%8E%E5%BE%80%E3%81%91%E3%80%8F%20-music%20clip-%20%EF%BC%88%E3%80%8E%E5%8A%87%E5%A0%B4%E7%89%88%20%E3%82%BD%E3%83%BC%E3%83%89%E3%82%A2%E3%83%BC%E3%83%88%E3%83%BB%E3%82%AA%E3%83%B3%E3%83%A9%E3%82%A4%E3%83%B3%20-%E3%83%97%E3%83%AD%E3%82%B0%E3%83%AC%E3%83%83%E3%82%B7%E3%83%96-%20%E6%98%9F%E3%81%AA%E3%81%8D%E5%A4%9C%E3%81%AE%E3%82%A2%E3%83%AA%E3%82%A2%E3%80%8F%E4%B8%BB%E9%A1%8C%E6%AD%8C%EF%BC%89%28mp3_320k%29.mp3?dl=0";
+
+         sounds[33]="https://dl.dropbox.com/s/26hgzd17p1ryuvj/lisa%20%E3%80%8E%E7%82%8E%E3%80%8F%20-music%20clip-%28mp3_320k%29.mp3?dl=0";
+
+         sounds[34]="https://dl.dropbox.com/s/cn0fxv0j3a51cpn/mrs.%20green%20apple%20-%20%E3%82%A4%E3%83%B3%E3%83%95%E3%82%A7%E3%83%AB%E3%83%8E%EF%BC%88inferno%EF%BC%89%28mp3_320k%29.mp3?dl=0";
+
+         sounds[35]="https://dl.dropbox.com/s/hrufl7n7g4ibirw/no%20game%20no%20life_%20zero%20op%20-%20there%20is%20a%20reason%20%28full%29%20_%20no%20song%20no%20life%28mp3_320k%29.mp3?dl=0";
+
+         sounds[36]="https://dl.dropbox.com/s/93r8gbpwwd6o99l/no%20pain_%20no%20game%20_%20%E3%83%8A%E3%83%8E%20music%20video%28mp3_320k%29.mp3?dl=0";
+
+         sounds[37]="https://dl.dropbox.com/s/v3oooshg21aqzm3/reona%20%E3%80%8E%E7%94%9F%E5%91%BD%E7%B7%9A%E3%80%8F-music%20video-%EF%BC%88%E6%9C%88%E5%A7%AB%20-a%20piece%20of%20blue%20glass%20moon-%20theme%20song%20e.p.%EF%BC%89%28mp3_320k%29.mp3?dl=0";
+
+sounds[37]="https://dl.dropbox.com/s/v3oooshg21aqzm3/reona%20%E3%80%8E%E7%94%9F%E5%91%BD%E7%B7%9A%E3%80%8F-music%20video-%EF%BC%88%E6%9C%88%E5%A7%AB%20-a%20piece%20of%20blue%20glass%20moon-%20theme%20song%20e.p.%EF%BC%89%28mp3_320k%29.mp3?dl=0";
+
+         sounds[38]="https://dl.dropbox.com/s/kgb645uxp5gsuca/supercell%20-%20kimino%20shiranai%20monogatari%28mp3_320k%29.mp3?dl=0";
+
+         sounds[39]="https://dl.dropbox.com/s/4j46qvluvh63vea/yoake%20to%20hotaru%E3%80%90%E5%A4%9C%E6%98%8E%E3%81%91%E3%81%A8%E8%9B%8D%E3%80%91_%20lyrics%28mp3_320k%29.mp3?dl=0";
+
+         sounds[40]="https://dl.dropbox.com/s/4jl2j410f10ttoc/yoasobi%20-%20%E7%BE%A4%E9%9D%92%20%20_%20the%20first%20take%28mp3_320k%29.mp3?dl=0";
+
+         sounds[41]="https://dl.dropbox.com/s/h0uu0sdo6kth1h4/%E3%81%8D%E3%81%BF%E3%81%AE%E5%90%8D%E5%89%8D%20_%20%E8%97%A4%E5%B7%9D%E5%8D%83%E6%84%9B%20%28%E3%80%8C%E7%9B%BE%E3%81%AE%E5%8B%87%E8%80%85%E3%81%AE%E6%88%90%E3%82%8A%E4%B8%8A%E3%81%8C%E3%82%8A%E3%80%8D%E3%82%A8%E3%83%B3%E3%83%87%E3%82%A3%E3%83%B3%E3%82%B0%E3%83%86%E3%83%BC%E3%83%9E%EF%BC%89music%20video%20%28__the%20rising%20of%20the%20shield%20hero__%20ending%20theme%20%29%28mp3_320k%29.mp3?dl=0";
+
+         sounds[42]="https://dl.dropbox.com/s/rr8v8l69u3ce7ha/%E3%82%B9%E3%83%91%E3%83%BC%E3%82%AF%E3%83%AB%20%5Boriginal%20ver.%5D%20-your%20name.%20music%20video%20edition-%20%E4%BA%88%E5%91%8A%E7%B7%A8%20from%20new%20album%E3%80%8C%E4%BA%BA%E9%96%93%E9%96%8B%E8%8A%B1%E3%80%8D%E5%88%9D%E5%9B%9E%E7%9B%A4dvd%28mp3_320k%29.mp3?dl=0";
+
+         sounds[43]="https://dl.dropbox.com/s/apdv9kgbcbqrf7z/%E3%82%84%E3%81%AA%E3%81%8E%E3%81%AA%E3%81%8E%20-%20%E3%83%A6%E3%82%AD%E3%83%88%E3%82%AD%20%28audio%29%28mp3_320k%29.mp3?dl=0";
+
+         sounds[44]="https://dl.dropbox.com/s/ep95qi9udiko1gw/%E3%82%84%E3%81%AA%E3%81%8E%E3%81%AA%E3%81%8E%C3%97the%20sixth%20lie%20_level%EF%BC%88mv_full%20ver.%29%28mp3_320k%29.mp3?dl=0";
+
+         sounds[45]="https://dl.dropbox.com/s/gxf9peqcouurc0e/%E3%83%AA%E3%82%BC%E3%83%AD2%E6%9C%9F%2040%E8%A9%B1%E6%8C%BF%E5%85%A5%E6%AD%8C%E3%80%8Cdoor%20%E3%80%8D%E3%82%A8%E3%83%9F%E3%83%AA%E3%82%A2%20%28%20cv_%E9%AB%98%E6%A9%8B%E6%9D%8E%E4%BE%9D%29%28mp3_320k%29.mp3?dl=0";
+
+         sounds[46]="https://dl.dropbox.com/s/calqy6llxkxgg80/%E5%BB%BB%E5%BB%BB%E5%A5%87%E8%AD%9A%20-%20eve%20mv%28mp3_320k%29.mp3?dl=0";
+
+         sounds[47]="https://dl.dropbox.com/s/9twrfhg6uqzviev/%E7%BE%8E%E6%B3%A2%E3%80%8Cmain%20actor%20%E3%80%8Dmv%28mp3_320k%29.mp3?dl=0";
+
+         sounds[48]="https://dl.dropbox.com/s/6kzoijkwqinwv1e/%E7%BE%8E%E6%B3%A2%E3%80%8C%E3%82%AB%E3%83%AF%E3%82%AD%E3%83%B2%E3%82%A2%E3%83%A1%E3%82%AF%E3%80%8Dmv%28mp3_320k%29.mp3?dl=0";
+
+         sounds[49]="https://dl.dropbox.com/s/sqkdi23d9g8t70p/%E7%BE%8E%E6%B3%A2%E3%80%8C%E3%83%A9%E3%82%A4%E3%83%A9%E3%83%83%E3%82%AF%E3%80%8Dmv%28mp3_320k%29.mp3?dl=0";
+
+         sounds[50]="https://dl.dropbox.com/s/wbw1oocbi29heqm/%E9%85%B8%E6%AC%A0%E5%B0%91%E5%A5%B3%E3%81%95%E3%83%A6%E3%82%8A%E3%80%8E%E5%B9%B3%E8%A1%8C%E7%B7%9A%E3%80%8Fmv%28%E3%83%95%E3%83%ABver%29%E3%82%A2%E3%83%8B%E3%83%A1%E3%80%8C%E3%82%AF%E3%82%BA%E3%81%AE%E6%9C%AC%E6%87%90%E3%80%8Ded%E3%83%86%E3%83%BC%E3%83%9E%28mp3_320k%29.mp3?dl=0";
+
+         sounds[51]="https://dl.dropbox.com/s/6yx8rkilru4i0cf/%E9%BA%BB%E6%9E%9D%20%E5%87%86%C3%97%E3%82%84%E3%81%AA%E3%81%8E%E3%81%AA%E3%81%8E%20%E3%80%8C%E7%B5%82%E3%82%8F%E3%82%8A%E3%81%AE%E4%B8%96%E7%95%8C%E3%81%8B%E3%82%89%E3%80%8D%28mp3_320k%29.mp3?dl=0";
+
+    
+
+         function getRandomSounds() {
+
+             var randomNum = Math.floor(Math.random() * sounds.length);
+
+             document.getElementById("RamMusik").src = sounds[randomNum];
+
+         }
+
+         
+
+         document.getElementById("RamMusik").addEventListener("ended", getRandomSounds);
+
+         getRandomSounds();               
+
+  
